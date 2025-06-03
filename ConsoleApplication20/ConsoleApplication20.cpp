e978e487c96359a05ddd3c4cad39373061031058
sergeMalove <student@tp.local>
2025-06-03 20:13:17 +0300

    Added constructors

--- ConsoleApplication20/ConsoleApplication20.cpp
+++ ConsoleApplication20/ConsoleApplication20.cpp
@@ -7,6 +7,23 @@ class Book {
     int realiseYear;
     int pagesQuantity;
     bool collectionEdition;
+
+public:
+
+    Book() {
+        name = "No name";
+        author = "Unknown author";
+        genre = "No genre";
+        realiseYear = 0;
+        pagesQuantity = 0;
+        collectionEdition = false;
+    }
+
+    Book(const std::string& name, const std::string& author, const std::string& genre, int realiseYear, int pagesQuantity, bool collectionEdition)
+        : name(name), author(author), genre(genre), realiseYear(realiseYear), pagesQuantity(pagesQuantity), collectionEdition(collectionEdition)
+    {
+    }
+
 };
 
 int main()
