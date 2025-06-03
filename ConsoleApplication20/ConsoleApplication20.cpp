a1c8488af82a1c1ff7f3d225e7fd7394483e600c
student <student@tp.local>
2025-06-03 20:24:18 +0300

    Animal

--- ConsoleApplication20/ConsoleApplication20.cpp
+++ ConsoleApplication20/ConsoleApplication20.cpp
@@ -1,6 +1,14 @@
-﻿#include <iostream>
+﻿#include "Animal.h"
+#include <iostream>
 
-int main()
-{
-    
+int main() {
+    Animal lion("Lion", 5);
+    lion.showInfo();
+
+    std::cout << "\nAttack possible: " << (lion.attack() ? "Yes" : "No") << std::endl;
+    std::cout << "Need food: " << lion.weNeedFood() << std::endl;
+
+    lion.showInfo();
+
+    return 0;
 }
