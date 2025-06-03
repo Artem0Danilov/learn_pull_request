ccf306c753d1a267ad4fa0e5c04086447030781b
alex <student@tp.local>
2025-06-03 20:27:41 +0300

    Redact method

--- ConsoleApplication20/ConsoleApplication20.cpp
+++ ConsoleApplication20/ConsoleApplication20.cpp
@@ -2,6 +2,9 @@
 
 int main()
 {
-    Book b1("Master and Margarita", "M. Bulgakov", 2001, "-");
-    Book b2("Crime and punishment", "F. Dostoyevskiy", 1992, "Old book; cover needs fixing");
+    Book b1("Master and Margarita", "M. Bulgakov", 2005, "In good condition");
+    Book b2("Crime and punishment", "F. Dostoyevskiy", 1982, "Old book; cover needs fixing");
+
+    b1.showInfo();
+    b2.showInfo();
 }
--- ConsoleApplication20/class.h
+++ ConsoleApplication20/class.h
@@ -11,4 +11,5 @@ public:
 	Book(std::string name, std::string author, int year, std::string notes)
 		: name(name), author(author), year(year), notes(notes){}
 	void showInfo();
+	void changeNotes(std::string newNotes);
 };
