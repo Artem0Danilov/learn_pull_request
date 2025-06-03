d0a9ab8b4b9ed374afa350f10ee23ad1a9326fc9
alex <student@tp.local>
2025-06-03 19:59:46 +0300

    Add method changeNotes

--- ConsoleApplication20/class.cpp
+++ ConsoleApplication20/class.cpp
@@ -8,3 +8,29 @@ void Book::showInfo() {
 	std::cout << "Extra notes: " << notes << std::endl;
 	std::cout << "------------\n";
 }
+
+void Book::changeNotes(std::string newNotes) {
+	int userChoice;
+	std::cout << "How do you want to change the notes?\n";
+	std::cout << "1. Add notes\n";
+	std::cout << "2. Rewrite notes\n";
+	std::cout << "3. Delete notes\n";
+	std::cout << ">>> ";
+	std::cin >> userChoice;
+	if (userChoice == 1) {
+		notes = notes + " " + newNotes;
+		std::cout << "Changed! Notes for this book are now: " << notes << std::endl;
+	}
+	else if (userChoice == 2) {
+		notes = newNotes;
+		std::cout << "Changed! Notes for this book are now: " << notes << std::endl;
+	}
+	else if (userChoice == 3) {
+		notes = "-";
+		std::cout << "Changed! Notes for this book are now: " << notes << std::endl;
+	}
+	else {
+		std::cout << "Error: unknown number.\n";
+	}
+
+}
