57222a7af30dbecb8c87e85ffda6c3122b764e42
student <student@tp.local>
2025-06-03 20:26:31 +0300

    alig

--- ConsoleApplication20/Animal.h
+++ ConsoleApplication20/Animal.h
@@ -1,1 +1,24 @@
-#pragma once
+#ifndef ANIMAL_H
+#define ANIMAL_H
+
+#include <string>
+
+class Animal {
+private:
+    std::string typeName_;
+    int age_;
+    bool isAlive_;
+    bool isHungry_;
+    bool isWild_;
+    int inThePack_;
+
+public:
+    Animal(const std::string& typeName, int age);
+
+    int weNeedFood();
+    bool attack() const;
+    void showInfo();
+
+};
+
+#endif
