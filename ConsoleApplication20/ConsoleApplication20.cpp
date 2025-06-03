51dd2f7eb177d6efe6da3d1c61d3fdc65f14958c
sergeMalove <student@tp.local>
2025-06-03 20:12:17 +0300

    Created Book class

--- ConsoleApplication20/ConsoleApplication20.cpp
+++ ConsoleApplication20/ConsoleApplication20.cpp
@@ -1,5 +1,14 @@
 ﻿#include <iostream>
 
+class Book {
+    std::string name;
+    std::string author;
+    std::string genre;
+    int realiseYear;
+    int pagesQuantity;
+    bool collectionEdition;
+};
+
 int main()
 {
     
